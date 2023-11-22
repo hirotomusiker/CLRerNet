ce265648b794bde63902213793ca946e7eb34649
hirotomusiker <hirotoschwert@gmail.com>
2023-11-22 06:36:13 +0000

    modify configs for training

--- configs/clrernet/culane/clrernet_culane_dla34.py
+++ configs/clrernet/culane/clrernet_culane_dla34.py
@@ -1,23 +1,41 @@
 _base_ = [
-    '../base_clrernet.py',
-    'dataset_culane_clrernet.py',
-    '../../_base_/default_runtime.py',
+    "../base_clrernet.py",
+    "dataset_culane_clrernet.py",
+    "../../_base_/default_runtime.py",
 ]
 
 # custom imports
 custom_imports = dict(
     imports=[
-        'libs.models',
-        'libs.datasets',
-        'libs.core.anchor',
+        "libs.models",
+        "libs.datasets",
+        "libs.core.bbox",
+        "libs.core.anchor",
+        "libs.core.hook",
     ],
     allow_failed_imports=False,
 )
 
-cfg_name = 'clrernet_culane_dla34.py'
+cfg_name = "clrernet_culane_dla34.py"
 
-model = dict(
-    test_cfg=dict(
-        conf_threshold=0.41
-    )
+model = dict(test_cfg=dict(conf_threshold=0.41))
+
+total_epochs = 15
+evaluation = dict(interval=3)
+checkpoint_config = dict(interval=total_epochs)
+
+data = dict(samples_per_gpu=24)  # single GPU setting
+
+# optimizer
+optimizer = dict(type="AdamW", lr=6e-4)
+optimizer_config = dict(grad_clip=None)
+
+# learning policy
+lr_config = dict(policy="CosineAnnealing", min_lr=0.0, by_epoch=False)
+
+log_config = dict(
+    hooks=[
+        dict(type="TextLoggerHook"),
+        dict(type="TensorboardLoggerHookEpoch"),
+    ]
 )
--- configs/clrernet/culane/clrernet_culane_dla34_ema.py
+++ configs/clrernet/culane/clrernet_culane_dla34_ema.py
@@ -1,17 +1,42 @@
 _base_ = [
-    '../base_clrernet.py',
-    'dataset_culane_clrernet.py',
-    '../../_base_/default_runtime.py',
+    "../base_clrernet.py",
+    "dataset_culane_clrernet.py",
+    "../../_base_/default_runtime.py",
 ]
 
 # custom imports
 custom_imports = dict(
     imports=[
-        'libs.models',
-        'libs.datasets',
-        'libs.core.anchor',
+        "libs.models",
+        "libs.datasets",
+        "libs.core.anchor",
     ],
     allow_failed_imports=False,
 )
 
-cfg_name = 'clrernet_culane_dla34_ema.py'
+cfg_name = "clrernet_culane_dla34_ema.py"
+
+model = dict(test_cfg=dict(conf_threshold=0.43))
+
+
+custom_hooks = [dict(type="ExpMomentumEMAHook", momentum=0.0001, priority=49)]
+
+total_epochs = 50
+evaluation = dict(interval=total_epochs)
+checkpoint_config = dict(interval=total_epochs)
+
+data = dict(samples_per_gpu=24)  # single GPU setting
+
+# optimizer
+optimizer = dict(type="AdamW", lr=6e-4)
+optimizer_config = dict(grad_clip=None)
+
+# learning policy
+lr_config = dict(policy="CosineAnnealing", min_lr=6e-4, by_epoch=False)
+
+log_config = dict(
+    hooks=[
+        dict(type="TextLoggerHook"),
+        dict(type="TensorboardLoggerHookEpoch"),
+    ]
+)
