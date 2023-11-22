2d212aa57fae8f69c4cc95813d21e428a812d88f
hirotomusiker <hirotoschwert@gmail.com>
2023-11-22 06:11:02 +0000

    modify laneiou loss and cost

--- tests/test_models/test_losses.py
+++ tests/test_models/test_losses.py
@@ -5,12 +5,12 @@ from libs.models.losses import CLRNetIoULoss, LaneIoULoss
 
 
 def test_laneiou_loss():
-    iou_loss = LaneIoULoss()
+    iou_loss = LaneIoULoss(img_h=320, img_w=1640)
     data = np.load("tests/data/test_iou_data.npz")
     target = torch.Tensor(data["target"])
     pred = torch.Tensor(data["pred"][: len(target)])
     expected = torch.tensor(1.51706361770)
-    loss = iou_loss(pred, target, img_h=320, img_w=1640)
+    loss = iou_loss(pred, target)
     assert expected.allclose(loss)
 
 
