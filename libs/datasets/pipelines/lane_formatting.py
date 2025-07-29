7c5faf2f54768c5ff5e086204e96efeb1025a2a8
hirotomusiker <hirotoschwert@gmail.com>
2025-07-29 04:29:15 +0000

    laneatt dataset modules

--- libs/datasets/pipelines/lane_formatting.py
+++ libs/datasets/pipelines/lane_formatting.py
@@ -14,14 +14,14 @@ from libs.utils.lane_utils import sample_lane
 class PackCLRNetInputs(BaseTransform):
     def __init__(
         self,
-        #keys=None,
+        # keys=None,
         meta_keys=None,
         max_lanes=4,
         num_points=72,
         img_w=800,
         img_h=320,
     ):
-        #self.keys = keys
+        # self.keys = keys
         self.meta_keys = meta_keys
         self.max_lanes = max_lanes
         self.n_offsets = num_points
@@ -36,7 +36,10 @@ class PackCLRNetInputs(BaseTransform):
         old_lanes = filter(lambda x: len(x) > 2, old_lanes)
 
         lanes = (
-            np.ones((self.max_lanes, 2 + 1 + 1 + 2 + self.n_offsets), dtype=np.float32)
+            np.ones(
+                (self.max_lanes, 2 + 1 + 1 + 2 + self.n_offsets),
+                dtype=np.float32,
+            )
             * -1e5
         )
         lanes[:, 0] = 1
@@ -74,6 +77,78 @@ class PackCLRNetInputs(BaseTransform):
             lanes[lane_idx, 4] = theta_far  # theta
             lanes[lane_idx, 5] = len(xs_inside_image)  # length
             lanes[lane_idx, 6 : 6 + len(all_xs)] = all_xs  # xs, absolute
+
+        results["lanes"] = to_tensor(lanes)
+        return results
+
+    def transform(self, results):
+        data = {}
+        img_meta = {}
+        data_sample = DetDataSample()
+        instance_data = InstanceData()
+        if "img" in results:
+            img = results["img"]
+            img = to_tensor(img).permute(2, 0, 1).contiguous()
+        if "lanes" in self.meta_keys:  # training
+            results = self.convert_targets(results)
+        for key in self.meta_keys:
+            img_meta[key] = results[key]
+        data_sample.gt_instances = instance_data
+        data_sample.set_metainfo(img_meta)
+        data["data_samples"] = data_sample
+        data["inputs"] = img
+        return data
+
+
+@TRANSFORMS.register_module()
+class PackLaneATTInputs(BaseTransform):
+    def __init__(
+        self,
+        # keys=None,
+        meta_keys=None,
+        max_lanes=4,
+        num_points=72,
+        img_w=640,
+        img_h=360,
+    ):
+        # self.keys = keys
+        self.meta_keys = meta_keys
+        self.max_lanes = max_lanes
+        self.n_offsets = num_points
+        self.n_strips = num_points - 1
+        self.strip_size = img_h / self.n_strips
+        self.offsets_ys = np.arange(img_h, -1, -self.strip_size)
+        self.img_w = img_w
+
+    def convert_targets(self, results):
+        old_lanes = results["gt_points"]
+        # removing lanes with less than 2 points
+        old_lanes = filter(lambda x: len(x) > 2, old_lanes)
+
+        lanes = (
+            np.ones(
+                (self.max_lanes, 2 + 1 + 2 + self.n_offsets), dtype=np.float32
+            )
+            * -1e5
+        )
+        lanes[:, 0] = 1
+        lanes[:, 1] = 0
+        for lane_idx, lane in enumerate(old_lanes):
+            try:
+                xs_outside_image, xs_inside_image = sample_lane(
+                    lane, self.offsets_ys, self.img_w
+                )
+            except AssertionError:
+                continue
+            if len(xs_inside_image) <= 1:  # to calculate theta
+                continue
+            all_xs = np.hstack((xs_outside_image, xs_inside_image))
+            lanes[lane_idx, 0] = 0
+            lanes[lane_idx, 1] = 1
+            lanes[lane_idx, 2] = len(xs_outside_image) / self.n_strips
+            lanes[lane_idx, 3] = xs_inside_image[0]  # x0, absolute
+            lanes[lane_idx, 4] = len(xs_inside_image)  # length
+            lanes[lane_idx, 5 : 5 + len(all_xs)] = all_xs  # xs, absolute
 
         results["lanes"] = to_tensor(lanes)
         return results
